335cb491d54e113c3db5b0dbf6525fd8c3d1314d
laithanhhung <laithanhhung233@gmail.com>
2026-03-09 13:09:22 +0700

    add vercel condig

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind()],
+  integrations: [tailwind()],
+  adapter: vercel({
+    imageService: true, // Enables Vercel's Image Optimization API for <CldImage />
+  }),
 });
